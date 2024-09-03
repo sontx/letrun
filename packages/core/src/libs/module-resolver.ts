65d240f7a5fe545d06c89d72a22bf88482fc1c40
sontx <xuanson33bk@gmail.com>
2024-09-03 21:19:10 +0700

    Load native node module without any further work

--- packages/core/src/libs/module-resolver.ts
+++ packages/core/src/libs/module-resolver.ts
@@ -9,22 +9,33 @@ export type ModuleResolverFn = <T = any>(modulePath: string) => Promise<T>;
  * Class responsible for resolving exported default entry point of node module from module directory or file path.
  */
 export class ModuleResolver {
+  constructor(private readonly rootDir = getEntryPointDir()) {}
+
   /**
    * Resolves a module or file path to its corresponding module.
    * @param {string} modulePath - The path to the module or file.
    * @returns {Promise<T>} - The resolved module.
    */
   resolve: ModuleResolverFn = async <T = any>(modulePath: string): Promise<T> => {
-    const effectivePath = isRelativePath(modulePath) ? path.resolve(getEntryPointDir(), modulePath) : modulePath;
+    const effectivePath = isRelativePath(modulePath) ? path.resolve(this.rootDir, modulePath) : modulePath;
     if (await this.isFile(effectivePath)) {
       const moduleType = effectivePath.endsWith('.cjs') ? 'commonjs' : 'module';
       return await this.resolveFile(effectivePath, moduleType);
     }
-    return await this.resolveModule(effectivePath);
+    return this.isInsideNodeModules(effectivePath)
+      ? await this.resolveInsideNodeModules(effectivePath)
+      : await this.resolveOutsideNodeModules(effectivePath);
   };
 
   private async resolveFile<T = any>(filePath: string, type: 'commonjs' | 'module'): Promise<T> {
     const obj = await this.dynamicImport(`file://${filePath}`);
+    return this.resolveDefaultExport(obj, type);
+  }
+
+  private resolveDefaultExport<T = any>(obj: any, type: 'commonjs' | 'module'): T {
+    if (!['commonjs', 'module'].includes(type)) {
+      throw new Error(`Unsupported module type: ${type}`);
+    }
     return type === 'commonjs' ? obj.default?.default : obj.default;
   }
 
@@ -32,15 +43,38 @@ export class ModuleResolver {
     return await import(filePath);
   }
 
-  private async resolveModule<T = any>(modulePath: string): Promise<T> {
+  private async resolveOutsideNodeModules<T = any>(modulePath: string): Promise<T> {
     const packageJson = await this.readPackageJson(modulePath);
     const main = packageJson.main ?? 'index.js';
     let mainPath = path.join(modulePath, main);
     if (!packageJson.main && !fs.existsSync(mainPath)) {
       mainPath = path.join(modulePath, 'index.cjs');
     }
-    const type = packageJson.type || 'commonjs';
+    const type = await this.getModuleType(packageJson);
     return await this.resolveFile(mainPath, type);
+  }
+
+  private async getModuleType(modulePathOrPackageJson: string | PackageJson) {
+    if (typeof modulePathOrPackageJson === 'string') {
+      const packageJson = await this.readPackageJson(modulePathOrPackageJson);
+      return packageJson.type || 'commonjs';
+    }
+    return modulePathOrPackageJson.type || 'commonjs';
+  }
+
+  private isInsideNodeModules(modulePath: string) {
+    return modulePath.includes('node_modules');
+  }
+
+  private async resolveInsideNodeModules(modulePath: string) {
+    const moduleName = this.getModuleNameFromNodeModulesPath(modulePath);
+    const obj = await this.dynamicImport(moduleName);
+    const moduleType = await this.getModuleType(modulePath);
+    return this.resolveDefaultExport(obj, moduleType);
+  }
+
+  private getModuleNameFromNodeModulesPath(modulePath: string) {
+    return path.relative(path.resolve(this.rootDir, 'node_modules'), modulePath);
   }
 
   private async readPackageJson(modulePath: string): Promise<PackageJson> {
