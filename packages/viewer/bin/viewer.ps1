f5250f04c95145c53bbd1c7921f982d9b338bbd1
sontx <xuanson33bk@gmail.com>
2024-10-17 18:47:43 +0700

    Add viewer package

---
+++ packages/viewer/bin/viewer.ps1
@@ -0,0 +1,17 @@
+#!/usr/bin/env pwsh
+
+$exe=""
+if ($PSVersionTable.PSVersion -lt "6.0" -or $IsWindows) {
+  # Fix case when both the Windows and Linux builds of Node
+  # are installed in the same directory
+  $exe=".exe"
+}
+$ret=0
+# Support pipeline input
+if ($MyInvocation.ExpectingInput) {
+  $input | & "node$exe"  "viewer.mjs" $args
+} else {
+  & "node$exe"  "viewer.mjs" $args
+}
+$ret=$LASTEXITCODE
+exit $ret
